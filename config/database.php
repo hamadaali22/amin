78d366de09a1ed0f0c4f034b5a23a1e8d6c22cd0
wasene <103010937+Ghaidtech@users.noreply.github.com>
2022-10-17 09:04:00 +0300

    upd

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'tameeni'),
+            'database' => env('DB_DATABASE', 'Admin-tameeni'),
             'username' => env('DB_USERNAME', 'Amin-jo'),
             'password' => env('DB_PASSWORD', '7&3x72vhA'),
             'unix_socket' => env('DB_SOCKET', ''),
